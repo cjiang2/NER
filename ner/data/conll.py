c546d161716da2ee1baa2d2a1c640bee8f3d2dfb
cjiang2 <zone3692@hotmail.com>
2022-02-27 14:52:40 -0700

    More stable dataloading codebase.

--- ner/data/conll.py
+++ ner/data/conll.py
@@ -2,11 +2,24 @@ import os
 
 import numpy as np
 
+# ##########
+# For Specific Datasets
+# ##########
+
+TAGS_CONLL03 = [
+    "POS",
+    "chunk",
+    "NER",
+]
+
+
+# ##########
+
 def load_data(
     filename: str, 
-    tag_ind: int, 
+    tags_to_add: list = TAGS_CONLL03, 
     lower_case: bool = True):
-    """Read dataset in CoNLL 2003 format.
+    """Read dataset in CoNLL 2003 format. Collect all tagging annotations, including NER.
     Reference:
         https://github.com/sgrvinod/a-PyTorch-Tutorial-to-Sequence-Labeling/blob/master/utils.py#L12
     Returns:
@@ -14,7 +27,10 @@ def load_data(
         tags: annotated tags.
     """
     words, tags = [], []
-    word_sample, tag_sample = [], []
+    word_sample = []
+    tag_sample = {}
+    for tag in tags_to_add:
+        tag_sample[tag] = []
 
     with open(filename, encoding='utf-8') as f:
         for line in f:
@@ -23,18 +39,22 @@ def load_data(
                 line = line.rstrip('\n').split()
                 
                 word_sample.append(line[0].lower() if lower_case else line[0])
-                tag_sample.append(line[tag_ind])
+                for i, tag in enumerate(tags_to_add):
+                    tag_sample[tag].append(line[i])
 
             # Sample at the end, append
             elif len(word_sample) > 0:
-                assert len(word_sample) == len(tag_sample)
+                for tag in tags_to_add: assert len(word_sample) == len(tag_sample[tag])
                 words.append(word_sample)
                 tags.append(tag_sample)
-                word_sample, tag_sample = [], []
+                word_sample = []
+                tag_sample = {}
+                for tag in tags_to_add:
+                    tag_sample[tag] = []
 
         # last sentence
         if len(word_sample) > 0:
-            assert len(word_sample) == len(tag_sample)
+            for tag in tags_to_add: assert len(word_sample) == len(tag_sample[tag])
             words.append(word_sample)
             tags.append(tag_sample)
 
@@ -42,3 +62,5 @@ def load_data(
     assert len(words) == len(tags)
 
     return words, tags
+
+
